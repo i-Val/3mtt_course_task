33653040b795d0f3eb2cc4d986637313c61d654a
i-val <ferdinantino1992@gmail.com>
2026-05-15 11:42:32 +0100

    Added prisma schema update after downgrading to version 6

--- node_lesson/src/server.js
+++ node_lesson/src/server.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
-const app = require('.app');
+const app = require('./app');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3005;
 
 
 app.listen(PORT, () => {
